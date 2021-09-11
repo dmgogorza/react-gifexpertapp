dcf6f599f2823bbfe28ee477156611d4092cfb9d
Diego Gogorza <dmgogorza@gmail.com>
2021-09-11 19:31:21 -0300

    Add docs

---
+++ docs/precache-manifest.3b704781a62c53404c09821f90e542a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df5fe619ee6023f3d89d593dee1f40d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0798f95174ccc3086340",
+    "url": "/static/css/main.33aa2db3.chunk.css"
+  },
+  {
+    "revision": "1ceee4cad2159c424706",
+    "url": "/static/js/2.a196fa71.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.a196fa71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0798f95174ccc3086340",
+    "url": "/static/js/main.dd444047.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
